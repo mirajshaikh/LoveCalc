c6cd26b43714ca19aadb2bc9ba47174bdc45f796
miraj <mirajpc@gmail.com>
2020-02-02 23:35:25 +0530

    added index file and script

---
+++ script.js
@@ -0,0 +1,17 @@
+var name1 = ("miraj")
+var name2 = ("shaikh")
+
+var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://love-calculator.p.rapidapi.com/getPercentage?fname=" + name1 + "&sname="+ name2,
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "love-calculator.p.rapidapi.com",
+		"x-rapidapi-key": "5f9fbe5894msh8704fe51688ba10p129888jsndc791e2a21ab"
+	}
+}
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
