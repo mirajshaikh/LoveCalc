cfa2bb8b82973087beaa81ce2875575cebad98bb
miraj-from-office <mirajpc@gmail.com>
2020-02-03 21:10:48 +0530

    add background color change when the percentage is change

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
-//$("#calc").click(function(){
-	//console.log("hello");
-/*$.ajax({
-url: "https://love-calculator.p.rapidapi.com/getPercentage?fname=miraj&sname=shaikh",
-dataType: 'json',
-success: function(results){
-console.log(results);
-}
-});*/
-//});
-$(document).ready(function() {
-	$("#calc").click(function(){
-		var name1 = $("#name1").val();
-		var name2 = $("#name2").val();
-	var settings = {
-		"async": true,
-		"crossDomain": true,
-		"url": "https://love-calculator.p.rapidapi.com/getPercentage?fname="+name1+"&sname="+name2,
-		"method": "GET",
-		"headers": {
-			"x-rapidapi-host": "love-calculator.p.rapidapi.com",
-			"x-rapidapi-key": "5f9fbe5894msh8704fe51688ba10p129888jsndc791e2a21ab"
-		}
-	}
-	
-	$.ajax(settings).done(function (response) {
-		console.log(response);
+$(document).ready(function () {
+		$("#calc").click(function () {
+			var name1 = $("#name1").val();
+			var name2 = $("#name2").val();
+			if (name1=="" || name2==""){
+				alert("Name is not Valid");
+			}
+			else{
+				$.ajax("https://love-calculator.p.rapidapi.com/getPercentage?fname="+name1+"&sname="+name2, {
+					"method": "GET",
+					"headers": {
+						"x-rapidapi-host": "love-calculator.p.rapidapi.com",
+						"x-rapidapi-key": "5f9fbe5894msh8704fe51688ba10p129888jsndc791e2a21ab"
+					}
+				})
+				.then(response => {
+					console.log(response.percentage+"%"); 
+					console.log(response.result);
+					var backcolor  = (response.percentage);
+					console.log(backcolor);	
+					if(backcolor > 40){
+						$("body").css("background-color", "pink");
+					  } else if(backcolor >= 6 && backcolor <= 40){
+						$("body").css("background-color", "blue");
+					  } else if(backcolor > 9){
+						$("body").css("background-color", "pink");
+					  } 				 
+				})
+				};
+		
+		});  
 	});
-});
-});
