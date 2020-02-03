4275d920e012312a614c3c3bcc84b54e367eb2a6
miraj-from-office <mirajpc@gmail.com>
2020-02-03 13:04:53 +0530

    solved problem

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
-var name1 = ("miraj")
-var name2 = ("shaikh")
-
-var settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://love-calculator.p.rapidapi.com/getPercentage?fname=miraj&sname=Alice",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "love-calculator.p.rapidapi.com",
-		"x-rapidapi-key": "5f9fbe5894msh8704fe51688ba10p129888jsndc791e2a21ab"
-	}
+//$("#calc").click(function(){
+	//console.log("hello");
+/*$.ajax({
+url: "https://love-calculator.p.rapidapi.com/getPercentage?fname=miraj&sname=shaikh",
+dataType: 'json',
+success: function(results){
+console.log(results);
 }
-
-$.ajax(settings).done(function (response) {
-	console.log(response);
+});*/
+//});
+$(document).ready(function() {
+	$("#calc").click(function(){
+		var name1 = $("#name1").val();
+		var name2 = $("#name2").val();
+	var settings = {
+		"async": true,
+		"crossDomain": true,
+		"url": "https://love-calculator.p.rapidapi.com/getPercentage?fname="+name1+"&sname="+name2,
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "love-calculator.p.rapidapi.com",
+			"x-rapidapi-key": "5f9fbe5894msh8704fe51688ba10p129888jsndc791e2a21ab"
+		}
+	}
+	
+	$.ajax(settings).done(function (response) {
+		console.log(response);
+	});
+});
 });
